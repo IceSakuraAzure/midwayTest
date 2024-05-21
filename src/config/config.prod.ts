5194dcf95c2f1ed76acf76a5197c8134f718086a
liqi <137158095@qq.com>
2024-05-21 09:35:15 +0800

    first commit 20210521

---
+++ src/config/config.prod.ts
@@ -0,0 +1,12 @@
+import { MidwayConfig } from '@midwayjs/core';
+
+export default {
+  // use for cookie sign key, should change to your own and keep security
+  keys: '1715741549626_7909',
+  koa: {
+    port: 7001,
+  },
+  midwayLogger: {
+    level:"debug",
+  },
+} as MidwayConfig;
